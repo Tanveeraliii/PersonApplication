a61cd53a836c5d08956f2b14f4c0694867b4c78e
Tanveeraliii <tanveer.ali.at@gmail.com>
2023-08-18 20:53:28 -0500

    Car POJO Implemented

--- Person/src/main/java/org/example/Main.java
+++ Person/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Person ob = new Person("Salah Uddin Sahebbb", 67);
 
-        Car car = new Car("ford", "2021");
+        Car car = new Car("ford", 2021);
         car.displayInfo();
 
     }
